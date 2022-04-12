ccfc55d371f7b1953bef387ae1a61f0d59ce91ad
Nick Khalow <71646502+NickKhalow@users.noreply.github.com>
2022-04-13 02:49:59 +0800

    Implicit converting T to EventValue<T>

--- Packages/EasyTools/Runtime/EventValue.cs
+++ Packages/EasyTools/Runtime/EventValue.cs
@@ -27,5 +27,12 @@ namespace NickKhalow.Util
         /// </summary>
         /// <returns>Value of EventValue type.</returns>
         public static implicit operator T(EventValue<T> eventValue) => eventValue.Value;
+
+        /// <summary>
+        /// Allows implicitly converts value to EventValue shell with it.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>EventValue shell.</returns>
+        public static implicit operator EventValue<T>(T value) => new EventValue<T>(value);
     }
 }
